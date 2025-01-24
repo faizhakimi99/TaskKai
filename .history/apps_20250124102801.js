cd1e38bebf41799d5b48704341ca74dfa1c4b5f5
Mohamad Faiz Hakimi <faizhakimi91@gmail.com>
2025-01-24 17:42:52 +0800

    24/01/2025 update

---
+++ .history/apps_20250124102801.js
@@ -0,0 +1,71 @@
+import * as dotenv from "dotenv"
+import createError from 'http-errors'
+dotenv.config()
+
+import express from "express"
+import ejs from 'ejs'
+import bodyParser from "body-parser"
+import path from "path"
+import { fileURLToPath } from "url"
+import session from "express-session"
+import routes from "./src/routes/index.js"
+
+// Fix __dirname for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+var app = express()
+
+// Sessions setup
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// view engine setup
+app.engine('html', ejs.renderFile)
+app.set('views', path.join(__dirname, 'src', 'views'))
+app.set('view engine', 'html')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(routes)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // var sess = req.session;
+
+  //console.log(err.status + "-" + err.message); 
+  // render the error page
+  res.status(err.status || 500);
+  if( err.status != null ){
+      res.render('error', { title: req.app.locals.tabTitle }, function (err, html) {
+          // html = utility.utilityJS(html, sess);
+          res.send(html);
+      });
+  }
+  else{
+      var resultData = {};
+      resultData.message = err.message;
+      if(!res.headersSent){
+          res.json(resultData);
+      }    
+  }
+  
+});
+
+var port = 6001
+app.listen(port, () => {
+  console.log("taskKai is running on port: " + port);
+})
--- apps.js
+++ apps.js
@@ -28,9 +28,13 @@ app.engine('html', ejs.renderFile)
 app.set('views', path.join(__dirname, 'src', 'views'))
 app.set('view engine', 'html')
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/',express.static(path.join(__dirname, 'src/public')))
+
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use(routes)
 
 // catch 404 and forward to error handler
@@ -42,21 +46,21 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  var sess = req.session;
+  // var sess = req.session;
 
   //console.log(err.status + "-" + err.message); 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   if( err.status != null ){
       res.render('error', { title: req.app.locals.tabTitle }, function (err, html) {
-          html = utility.utilityJS(html, sess);
-          res.send(html);
+          // html = utility.utilityJS(html, sess)
+          res.send(html)
       });
   }
   else{
-      var resultData = {};
+      var resultData = {}
       resultData.message = err.message;
       if(!res.headersSent){
           res.json(resultData);
